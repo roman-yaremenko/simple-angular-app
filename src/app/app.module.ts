233341fae867bec32c2f231ad3d87ac170f838b7
Roman Yaremenko <romen.yaremenko@gmail.com>
2018-07-23 21:48:50 +0300

    fixed comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-
 import { AppComponent } from './app.component';
+
+import { EntitiesModule} from './modules/entities/entities.module';
 
 @NgModule({
   declarations: [
@@ -11,9 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule, 
-    MatCheckboxModule
+    EntitiesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/entities/entities-dashboard/entities-dashboard.component.ts
+++ src/app/modules/entities/entities-dashboard/entities-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-entities-dashboard',
   templateUrl: './entities-dashboard.component.html',
