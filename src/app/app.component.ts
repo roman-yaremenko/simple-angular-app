8e1bda71da3abc7127d92f3c128c1bbdcc48834a
Roman Yaremenko <romen.yaremenko@gmail.com>
2018-07-26 16:46:53 +0300

    Created round button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'simple-angular-app';
-  faCoffee = faCoffee;
+  faCoffee = faPlus;
 }
--- src/app/modules/entities/entities-dialog-connect/entities-dialog-connect.component.ts
+++ src/app/modules/entities/entities-dialog-connect/entities-dialog-connect.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-entities-dialog-connect',
   templateUrl: './entities-dialog-connect.component.html',
-  styleUrls: ['./entities-dialog-connect.component.css']
+  styleUrls: ['./entities-dialog-connect.component.scss']
 })
 export class EntitiesDialogConnectComponent implements OnInit {
 
